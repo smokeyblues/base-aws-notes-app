a5d49934793a887770d99e15797f50e66c43e9ef
smokeyblues <aaronpennington@Aarons-Mac-mini.local>
2024-11-30 16:41:17 -0700

    Setting up React app

--- packages/scripts/sst-env.d.ts
+++ packages/scripts/sst-env.d.ts
@@ -10,6 +10,10 @@ declare module "sst" {
       "type": "sst.aws.ApiGatewayV2"
       "url": string
     }
+    "Frontend": {
+      "type": "sst.aws.StaticSite"
+      "url": string
+    }
     "IdentityPool": {
       "id": string
       "type": "sst.aws.CognitoIdentityPool"
